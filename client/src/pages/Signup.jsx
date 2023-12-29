014777c955c916cbe624e8248c8138874fb610a7
Albert Legacki <albert.legacki21@my.stjohns.edu>
2023-12-29 15:16:07 -0500

    Complete sign-in page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,11 +1,11 @@
-
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Signup() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -14,7 +14,7 @@ export default function Signup() {
     try {
       setLoading(true);
       setError(false);
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -28,6 +28,7 @@ export default function Signup() {
         setError(true);
         return;
       }
+      navigate('/sign-in');
     } catch (error) {
       setLoading(false);
       setError(true);
@@ -59,19 +60,20 @@ export default function Signup() {
           onChange={handleChange}
         />
         <button
+        disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg 
         uppercase hover:opacity-95 disabled:opacity-80"
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
-      <div className="flex gap-2 mt-5">
+      <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to="/sign-in">
-          <span className="text-blue-500">Sign In</span>
+        <Link to='/sign-in'>
+          <span className='text-blue-500'>Sign in</span>
         </Link>
       </div>
-      <p className='text-red-700 mt-5'>{error && "Something went wrong!"}</p>
+      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
     </div>
   );
 }
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
     next(error);
   }
@@ -17,11 +17,11 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
-  try {
+ try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "User not found"));
+    if (!validUser) return next(errorHandler(404, 'User not found'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+    if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
     const expiryDate = new Date(Date.now() + 3600000);
@@ -32,4 +32,4 @@ export const signin = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-};
+  };
