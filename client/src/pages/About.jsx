a76673a1741762905ec20e4566326e598eb6871b
Albert Legacki <albert.legacki21@my.stjohns.edu>
2024-01-15 11:28:45 -0500

    Add home and About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
 export default function About() {
-  return (
-    <div>About</div>
-  )
-}
+    return (
+      <div className="px-4 py-12 max-w-2xl mx-auto">
+        <h1 className="text-3xl font-bold mb-4 text-slate-800">
+          About
+        </h1>
+       
+        <p className="mb-4 text-slate-700 ">
+          Participating in my first Full Stack MERN User Authentication project
+          was a pivotal step in my programming journey. As I explored React,
+          MongoDB, Express, and Node.js for the first time, I encountered
+          challenges that ultimately became valuable learning opportunities.
+          Overcoming hurdles in implementing secure login systems and handling
+          user sessions not only deepened my understanding of these technologies
+          but also boosted my problem-solving skills. The process of developing
+          this application not only provided me with hands-on experience but also
+          instilled a newfound confidence and a deeper interest in web
+          development. Looking ahead, I'm eager to explore more advanced features
+          and delve into areas like API integration to further enhance my skill
+          set in this dynamic field.
+        </p>
+        <p className="mb-4 text-slate-800 font-bold">
+          Thank you for visiting my User Authentication App!
+        </p>
+      </div>
+    );
+  }
+  
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        Welcome to my User Authentication App!
+      </h1>
+      <p className="mb-4 text-slate-700 ">
+        This web application is developed using the MERN (MongoDB, Express,
+        React, Node.js) stack, encompassing authentication functionalities.
+        Users can seamlessly register, log in, and log out, with exclusive
+        access granted to protected routes accessible only to authenticated
+        individuals.
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Utilizing React for the front-end and employing React Router for
+        client-side routing, this application offers a seamless user interface.
+        On the back-end, Node.js and Express work together, with MongoDB serving
+        as the database foundation. The implementation of authentication is
+        executed through the utilization of JSON Web Tokens (JWT), ensuring
+        secure and efficient user validation.
+      </p>
+      <p className="mb-4 text-slate-800  font-bold">
+        Feel free to have a look around!
+      </p>
+    </div>
+  );
 }
