0369299d1edb75baa4ba214696fe1ad548d3b77a
Albert Legacki <albert.legacki21@my.stjohns.edu>
2024-01-15 11:36:47 -0500

    Add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -16,6 +17,13 @@ mongoose
   });
 
 const app = express();
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'clien', 'dist', 'index.html'));
+});
 
 app.use(express.json());
 
